16abe52eea1629d5f5ed8473260aa7aa544d65d2
chhc0505 <70686425+chhc0505@users.noreply.github.com>
2023-10-01 12:30:22 +0900

    Update search.py

--- Text_Searching/search.py
+++ Text_Searching/search.py
@@ -26,12 +26,18 @@ class Trie:
                 return False
         return True
     
-if __name__ == "__main__":
-    input_text_list = ["풍경 묘사", "풍경 묘사 해줘", "풍경 묘사 좀 부탁해", "객체인식 좀 해줘"]
-    for input_text in input_text_list:
-        word_list = input_text.split()
-        MyTrie = Trie()
-        for word in word_list:
-            MyTrie.insert(word)
-        print(MyTrie.search("풍경") or MyTrie.search("묘사"),end=' ')
-        print(MyTrie.search("객체") or MyTrie.search("인식"))
+#if __name__ == "__main__":
+#    input_text_list = ["풍경 묘사", "풍경 묘사 해줘", "풍경 묘사 좀 부탁해", "객체인식 좀 해줘"]
+#    for input_text in input_text_list:
+#        word_list = input_text.split()
+#        MyTrie = Trie()
+#        for word in word_list:
+#            MyTrie.insert(word)
+#        print(MyTrie.search("풍경") or MyTrie.search("묘사"),end=' ')
+#        print(MyTrie.search("객체") or MyTrie.search("인식"))
+
+# Output
+# True False
+# True False
+# True False
+# False True
